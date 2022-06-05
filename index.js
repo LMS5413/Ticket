90c5f9c1e384dba3724050cc5bc6253d80f9a8e9
sebastianjnuwu <sebastianjnuwu@gmail.com>
2022-06-05 09:44:36 -0300

    deixando mais bonitinho

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  const ping = new Date();
-  ping.setHours(ping.getHours() - 3);
-  console.log(`Novo ping em: ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT); 
-
 const { Client, Collection } = require('discord.js');
 const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 const { token } = require('./config');
+client.login(token);
 const fs = require('fs');
+const colors = require('colors');
 
+client.once("ready", () => {
+ console.log(colors.cyan("[Info]")+` ${client.user.tag} foi iniciada em ${client.guilds.cache.size} sevidores!`);
+ console.log(colors.cyan("[Info]")+` tendo acesso a ${client.channels.cache.size} canais!`);
+ console.log(colors.cyan("[Info]")+` contendo ${client.users.cache.size} usuarios!`);
+});
 
 client.commands = new Collection();
 
-console.log("\n[FILE MANAGER ] Carregando o handler...");
+
 fs.readdir("./src/commands", (err, files) => {
     if (err) console.error(err);
     let arquivojs = files.filter(f => f.split(".").pop() === "js");
@@ -35,19 +32,15 @@ fs.readdir("./src/events/", (err, files) => {
     });
 });
 
-console.log("\n[FILE MANAGER ] Eventos e comandos carregados com sucesso!");
-console.log("\n[CLIENT] Ligando a aplicação...");
-
-client.login(token);
 
 process.on('unhandledRejection', error => {
     if (error.message === "An invalid token was provided.") {
-        return console.error("\n[ERRO unhandledRejection] Você providenciou um token invalido para sua aplicação.\n\nPara pegar um token válido veja esse tutorial: https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot.");
+        return console.error(colors.red("[Info]")+" Você providenciou um token invalido para sua aplicação para pegar um token válido veja esse tutorial: https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot.");
     }
-    console.error("\n[ERRO unhandledRejection] Ocorreu um erro na aplicação! Detalhes embaixo:");
+    console.error(colors.red("[Info]")+" Ocorreu um erro na aplicação! Detalhes embaixo:");
     console.error(error.stack);
 });
 process.on('uncaughtException', error => {
-    console.error("\n[ERRO uncaughtException] Ocorreu um erro na aplicação! Detalhes embaixo:");
+    console.error(colors.red("[Info]")+" Ocorreu um erro na aplicação! Detalhes embaixo:");
     console.error(error.stack);
 });
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const colors = require('colors');
 const { mysql } = require('../../config.json');
 const table = require('../../tables/ticket');
 const queryInterface = table.connect.getQueryInterface();
@@ -7,7 +8,7 @@ module.exports = {
     execute(client) {
         table.findOne({ where: { id: client.user.id } }).catch(e => {
             if (e.message === `Table \'${mysql.database}.tickets\' doesn't exist`) {
-                console.log(`[MYSQL] Não encontrei a tabela tickets! Criando uma nova...`);
+                console.log(colors.red("[Info]")+` Não encontrei a tabela tickets! Criando uma nova...`);
                 queryInterface.createTable('tickets', {
                     id: {
                         type: Sequelize.STRING,
@@ -18,9 +19,9 @@ module.exports = {
                         allowNull: false,
                     }
                 });
-   return console.log(`[Info] Tabela criada com sucesso no MYSQL!`);
+   return console.log(colors.cyan("[Info]")+` Tabela criada com sucesso no MYSQL!`);
 }
-   console.error(`[Info] Não foi possivel fazer uma conexão com o banco de dados! Erro: ${e.message}`);
+   console.error(colors.red("[Info]")+` Não foi possivel fazer uma conexão com o banco de dados! Erro: ${e.message}`);
         });
     },
 };
