74a4800df1aaf7b8b6f961659033dae86a0b6534
LMS5413 <brleonardo790@gmail.com>
2022-09-20 19:33:47 -0300

    Fixed category is not syns

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -17,7 +17,7 @@ module.exports = {
         if (interaction.isButton()) {
             if (interaction.customId === "fechar") {
                 if (!ticket) return interaction.deferUpdate()
-                await interaction.channel.permissionOverwrites.edit(interaction.user, {ViewChannel: false});
+                await interaction.channel.permissionOverwrites.edit(ticket.getDataValue('id'), {ViewChannel: false});
                 const embed = new EmbedBuilder()
                     .setTitle("Atendimento")
                     .setDescription("Atendimento fechado com sucesso!");
@@ -66,7 +66,7 @@ module.exports = {
                     break;
                 case "reabrir":
                     interaction.deferUpdate()
-                    await interaction.channel.permissionOverwrites.edit(ticket.id, { ViewChannel: true });
+                    await interaction.channel.permissionOverwrites.edit(ticket.getDataValue('id'), { ViewChannel: true });
                     interaction.message.delete();
                     break;
             }
@@ -77,10 +77,11 @@ module.exports = {
         const emojiReplace = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
         const category = (await departaments.findAll()).filter(x => x.getDataValue('id_guild') === interaction.guild.id).find(x => x.getDataValue('name').toLowerCase().replace(emojiReplace, "").trim() === interaction.values[0])
         const roleList = await roles.findAll({where: {id_guild: interaction.guild.id}})
+        console.log(category, interaction.values[0])
         let channel = await interaction.guild.channels.create({
             name: `${interaction.values[0]}-${interaction.user.username}`,
             type: ChannelType.GuildText,
-            parent: client.channels.cache.get(category?.category_id)?.id ?? null,
+            parent: client.channels.cache.get(category.getDataValue('category_id'))?.id ?? null,
             permissionOverwrites: [
                 {
                     id: client.user.id,
