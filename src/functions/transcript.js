da1ee69c039071b2f8d324762607b89831130c17
LMS5413 <brleonardo790@gmail.com>
2022-10-18 14:25:10 -0300

    Added recursive function for ticket transcript

--- src/functions/transcript.js
+++ src/functions/transcript.js
@@ -2,18 +2,18 @@ const { generateTranscript } = require('reconlx');
 
 async function transcript(channel, guild) {
     let msgs = [];
-    const getMsg = async (msg, limit=100) => {
-        const messages = await channel.messages.fetch({ limit: limit, before: msg ? msg.id : null });
-        msgs = msgs.concat(messages.array());
-        if (messages.size === limit) {
-            await getMsg(messages.last(), limit);
+    const getMsg = async (options = { limit: 100 }) => {
+        const messages = await channel.messages.fetch(options);
+        msgs = msgs.concat(Array.from(messages.values()));
+        if (messages.size === 100) {
+            await getMsg({ limit: 100, before: messages.last().id });
         }
     }
-    getMsg()
-    let transcript = await generateTranscript({ messages: msgs, guild: guild, channel: channel });
+    await getMsg()
+    let transcript = await generateTranscript({ messages: msgs.filter(x => !x.author.bot), guild: guild, channel: channel });
     let html = transcript.toString();
 
-    html = html.replace("<p class='copyright'>© github.com/reconlx</p>", "").replace("\n", "").replace(`Transcripted ${msgs.length} messages.`, `${msgs.length} mensagens foram escritas aqui.`);
+    html = html.replace("<p class='copyright'>© github.com/reconlx</p>", "").replace("\n", "").replace(`Transcripted ${msgs.filter(x => !x.author.bot).length} messages.`, `${msgs.filter(x => !x.author.bot).length} mensagens foram escritas aqui.`);
     transcript = Buffer.from(html, "utf-8");
 
     return transcript;
